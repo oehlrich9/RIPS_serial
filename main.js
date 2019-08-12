729926ee6740ca85473ba02fae9e82eda309e774
benedikt oehlrich <oehlrich9@googlemail.com>
2019-08-12 16:19:58 +0200

    Typo

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const SocketProvider = require('./SocketProvider');
 var config = JSON.parse(fs.readFileSync('config.json'));
 console.log(config.radios.length);
 var serialServers = [];
-var sockerProvider = new SockerProvider();
+var sockerProvider = new SocketProvider();
 
 config.radios.forEach(function(radio, id) {
     serialServers.push(new SerialServer(radio, sockerProvider));
